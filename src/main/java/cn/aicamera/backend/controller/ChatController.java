3e9035e86a9d8e0b0590ef20e92c7e89ec2bfdd6
Kcil <kkcil_nau@163.com>
2025-03-16 00:33:39 +0800

    feat: 完成了文案和拍照的其他功能，成功对接flask应用并打包为jar

--- src/main/java/cn/aicamera/backend/controller/ChatController.java
+++ src/main/java/cn/aicamera/backend/controller/ChatController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Flux;
 
+import java.io.IOException;
+import java.util.List;
+
 // TODO
 @RestController
 @RequestMapping("/chat")
@@ -18,12 +21,13 @@ public class ChatController {
     private ChatService chatService;
 
     @PostMapping("/send")
-    public ResponseEntity<SuccessResponse> sendMessage(
+    public ResponseEntity<GeneralResponse<String>> sendMessage(
             @RequestHeader("Authorization") String token,
             @RequestParam String message) {
-//        chatService.sendMessage(token, message);
         System.out.println("Receive message:"+message);
-        return ResponseEntity.ok(new SuccessResponse(true, "消息发送成功"));
+        String response=chatService.sendMessage(token, message);
+        System.out.println("Receive response:"+response);
+        return ResponseEntity.ok(new GeneralResponse<>(true, "消息发送成功", response));
     }
 
 //    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
@@ -32,11 +36,19 @@ public class ChatController {
 //    }
 
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<SuccessResponse> uploadImage(
+    public ResponseEntity<GeneralResponse<String>> uploadImage(
             @RequestHeader("Authorization") String token,
-            @RequestPart("image") MultipartFile image) {
-//        chatService.uploadImage(token, image);
-        System.out.println("Receive file: type "+image.getContentType()+" name "+image.getOriginalFilename());
-        return ResponseEntity.ok(new SuccessResponse(true, "图片上传成功"));
+            @RequestPart("images") List<MultipartFile> images,
+            @RequestPart("platform") String platform,
+            @RequestPart("mood") String mood) throws IOException {
+//        try {
+//            chatService.uploadImage(token, image);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            throw new RuntimeException(e);
+//        }
+        System.out.println("Receive file: size "+images.size()+" platform "+ platform + " mood " + mood);
+        String response= chatService.uploadImageRequest(token,images,platform,mood);
+        return ResponseEntity.ok(new GeneralResponse<>(true, "图片上传成功",response));
     }
 }
